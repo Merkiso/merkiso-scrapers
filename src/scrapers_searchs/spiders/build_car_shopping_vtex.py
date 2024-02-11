82e7d93f72ecd8e124d4788bd3b976ca6ea4aad4
German Castellanos <germancodeg@gmail.com>
2024-02-11 15:44:27 -0600

    fix(struct) bugs with folder struct

--- src/scrapers_searchs/spiders/build_car_shopping_vtex.py
+++ src/scrapers_searchs/spiders/build_car_shopping_vtex.py
@@ -4,9 +4,9 @@ import scrapy
 
 from string import Template
 from scrapy.crawler import CrawlerProcess
-from app.scrapers_searchs.utils.build_url import build_url
+from src.scrapers_searchs.utils.build_url import build_url
 
-from app.scrapers_searchs.structs.items_car_shop import BuildItems
+from src.scrapers_searchs.structs.items_car_shop import BuildItems
 
 
 
